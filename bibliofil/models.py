5f69c09d7ae18a6797b097fcf2dad0af926aa1c7
Sergey Skorokhod <sergeyskorokhod2@gmail.com>
2026-01-14 17:49:33 +0300

    update models

--- bibliofil/models.py
+++ bibliofil/models.py
@@ -3,13 +3,14 @@ from dataclasses import dataclass, replace
 
 @dataclass(slots=True)
 class BiblioFile:
+    id: int
     name: str
     extension: str
     path: str
     size: int
     md5: str | None = None
     created_at: str | None = None
-    parent_id: int | None = None
+    archive_id: int | None = None
     is_archive: int = 0
 
     def update(self, **changes):
@@ -17,12 +18,18 @@ class BiblioFile:
 
     def to_tuple(self):
         return (
-            self.name, self.extension, self.path, self.size,
-            self.md5, self.created_at, self.parent_id, self.is_archive
+            self.name,
+            self.extension,
+            self.path,
+            self.size,
+            self.md5,
+            self.created_at,
+            self.archive_id,
+            self.is_archive,
         )
 
     def __repr__(self):
-        return f"BiblioFile(name='{self.name}')"
+        return f"BiblioFile(id='{self.id}' name='{self.name}' size={self.size} md5='{self.md5}' archive_id={self.archive_id})')"
 
 
 @dataclass(slots=True)
@@ -30,4 +37,3 @@ class BiblioExtStat:
     extension: str
     count: int
     size: int
-    percent: float
